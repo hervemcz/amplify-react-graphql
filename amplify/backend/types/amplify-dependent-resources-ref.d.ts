4cdae2f487beba1ffe5f8d14ed42582bd5d90c9d
Herve <hervemcz@gmail.com>
2022-11-30 10:29:06 +0100

    Added Storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "herveamplifys3bucket": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
